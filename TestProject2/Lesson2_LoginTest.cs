7ff2dc182588c691e0aaede22a4ae3370625a256
Mikhail <bunnysband@gmail.com>
2018-10-27 12:46:10 +0300

    поправил путь

---
+++ TestProject2/Lesson2_LoginTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestProject2
+{
+    [TestFixture]
+    public class Lesson2_LoginTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        string userName = "admin";
+        string passWord = "admin";
+        string adminLoginUrl = "http://localhost/litecart/admin/";
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new FirefoxDriver("c:\\tools");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Url = adminLoginUrl;
+            driver.FindElement(By.Name("username")).SendKeys(userName);
+            driver.FindElement(By.Name("password")).SendKeys(passWord);
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ TestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40274195-dd34-41bf-bd62-67f1d8f8ea40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
