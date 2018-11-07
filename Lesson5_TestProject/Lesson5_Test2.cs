cf147eea70e6e17574c02a956cd90ea351306385
Mikhail Sokolov <38131178+bunnysband@users.noreply.github.com>
2018-11-07 10:21:20 +0300

    2й тест к 5й лекции

--- Lesson5_TestProject/Lesson5_Test2.cs
+++ Lesson5_TestProject/Lesson5_Test2.cs
@@ -7,6 +7,7 @@ using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 using SeleniumTestBase;
 using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -45,10 +46,10 @@ namespace Lesson5_TestProject
             string productNameOnMainPage = GetProductOnMainPageAttribute("div.name").Text;
             string regularPriceOnMainPage = GetProductOnMainPageAttribute(regularPriceCssSelector).Text;
             string campaignPriceOnMainPage = GetProductOnMainPageAttribute(campaignPriceCssSelector).Text;
-            int regularPriceOnMainPageFontSize = Int32.Parse(Regex.Match(GetProductOnMainPageAttribute(regularPriceCssSelector).GetCssValue("font-size"), "\\d{ 1,}").Value);
+            string regularPriceOnMainPageFontSize = GetProductOnMainPageAttribute(regularPriceCssSelector).GetCssValue("font-size");
             string regularPriceOnMainPageFontColor = GetProductOnMainPageAttribute(regularPriceCssSelector).GetCssValue("color");
-            int[] regularPriceOnMainPageFontStyle = Regex.Matches(GetProductOnMainPageAttribute(regularPriceCssSelector).GetCssValue("text-decoration-line"), "\\d{ 1,}").OfType<int>().ToArray();
-            int campaignPriceOnMainPageFontSize = Int32.Parse(Regex.Match(GetProductOnMainPageAttribute(campaignPriceCssSelector).GetCssValue("font-size"), "\\d{ 1,}").Value);
+            string regularPriceOnMainPageFontStyle = GetProductOnMainPageAttribute(regularPriceCssSelector).GetCssValue("text-decoration-line");
+            string campaignPriceOnMainPageFontSize = GetProductOnMainPageAttribute(campaignPriceCssSelector).GetCssValue("font-size");
             string campaignPriceOnMainPageFontColor = GetProductOnMainPageAttribute(campaignPriceCssSelector).GetCssValue("color");
             string campaignPriceOnMainPageFontStyle = GetProductOnMainPageAttribute(campaignPriceCssSelector).GetCssValue("font-weight");
 
@@ -64,9 +65,32 @@ namespace Lesson5_TestProject
             string campaignPriceOnProductPageFontColor = GetProductOnItsPageAttribute(campaignPriceCssSelector).GetCssValue("color");
             string campaignPriceOnProductPageFontStyle = GetProductOnItsPageAttribute(campaignPriceCssSelector).GetCssValue("font-weight");
 
+            regularPriceOnMainPageFontSize = Regex.Match(regularPriceOnMainPageFontSize, @"[0-9.,]{1,}").Value;
+            campaignPriceOnMainPageFontSize = Regex.Match(campaignPriceOnMainPageFontSize, @"[0-9.,]{1,}").Value;
+            regularPriceOnProductPageFontSize = Regex.Match(regularPriceOnMainPageFontSize, @"[0-9.,]{1,}").Value;
+            campaignPriceOnProductPageFontSize = Regex.Match(campaignPriceOnMainPageFontSize, @"[0-9.,]{1,}").Value;
+            var regColorOnMain = Regex.Matches(regularPriceOnMainPageFontColor, @"\d{1,}").OfType<Match>().Select(x => Convert.ToInt32(x.Value)).Take(3).ToArray();
+            var campColorOnMain = Regex.Matches(campaignPriceOnMainPageFontColor, @"\d{1,}").OfType<Match>().Select(x => Convert.ToInt32(x.Value)).Take(3).ToArray();
+            var regColorOnProduct = Regex.Matches(regularPriceOnProductPageFontColor, @"\d{1,}").OfType<Match>().Select(x => Convert.ToInt32(x.Value)).Take(3).ToArray();
+            var campColorOnProduct = Regex.Matches(campaignPriceOnProductPageFontColor, @"\d{1,}").OfType<Match>().Select(x => Convert.ToInt32(x.Value)).Take(3).ToArray();
+           
             Assert.AreEqual(productNameOnMainPage, productNameOnProductPage);
             Assert.AreEqual(regularPriceOnMainPage, regularPriceOnProductPage);
             Assert.AreEqual(campaignPriceOnMainPage, campaignPriceOnProductPage);
+            Assert.AreEqual(regularPriceOnMainPageFontStyle, "line-through");
+            Assert.AreEqual(regColorOnMain[0], regColorOnMain[1]);
+            Assert.AreEqual(regColorOnMain[0], regColorOnMain[2]);
+            Assert.AreEqual(campaignPriceOnMainPageFontStyle, "700");
+            Assert.AreEqual(campColorOnMain[1], 0);
+            Assert.AreEqual(campColorOnMain[2], 0);
+            Assert.AreEqual(regularPriceOnProductPageFontStyle, "line-through");
+            Assert.AreEqual(regColorOnProduct[0], regColorOnProduct[1]);
+            Assert.AreEqual(regColorOnProduct[0], regColorOnProduct[2]);
+            Assert.AreEqual(campaignPriceOnProductPageFontStyle, "700");
+            Assert.AreEqual(campColorOnProduct[1], 0);
+            Assert.AreEqual(campColorOnProduct[2], 0);
+            Assert.Greater(campaignPriceOnMainPageFontSize, regularPriceOnMainPageFontSize);
+            Assert.Greater(campaignPriceOnProductPageFontSize, regularPriceOnProductPageFontSize);
         }
     }
 }
